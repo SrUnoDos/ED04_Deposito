dc26cc7a346b4f3903c00bf55fafd1df81c9a0b0
juanf <juanf@DESKTOP-IS4BLVN>
2022-02-08 17:45:28 +0100

    Comentarios javadoc

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,7 +1,8 @@
 package cuentas;
 
 /**
- *
+ * Crea objetos de tipo CCuenta, con nombre, número de cuenta, saldo y tipos de interes.
+ * 
  * @author Juan Couto
  */
 public class CCuenta {
@@ -62,16 +63,42 @@ public class CCuenta {
         this.tipoInterés = tipoInterés;
     }
 
-
+    /**
+     * Nombre titular de la cuenta
+     */
     private String nombre;
+    
+    /**
+     * Numero de cuenta del cliente
+     */
     private String cuenta;
+    
+    /**
+     * Saldo disponible en la cuenta
+     */
     private double saldo;
+    
+    /**
+     * Tipo de interés aplicado en la cuenta
+     */
     private double tipoInterés;
-
+    
+    
+    /**
+     * Constructor por defecto de la clase CCuenta
+     */
     public CCuenta()
     {
     }
 
+    /**
+     * Constructor de la clase cuenta
+     * 
+     * @param nom nombre        
+     * @param cue cuenta        
+     * @param sal saldo         
+     * @param tipo tipo de interes
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -79,18 +106,35 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     * Método que devuelve el saldo de la cuenta
+     * 
+     * @return devuelve el saldo disponible en la cuenta
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * Metodo para insertar saldo en la cuenta
+     * 
+     * @param cantidad cantidad 
+     * @throws Exception Excepcion capturada si se intenta ingresar una cantidad negativa
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+    
+    /**
+     * Metodo para retirar saldo en la cuenta
+     * 
+     * @param cantidad cantidad 
+     * @throws Exception Excepcion capturada si se intenta retirar una cantidad negativa o superior al saldo disponible
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
